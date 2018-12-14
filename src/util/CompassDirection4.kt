7be7b2783abe610aa1117d28c1fa8b045c6cde42
Thiakil <xander@thiakil.com>
2018-12-14 10:27:09 +0800

    fix rotate derp

--- src/util/CompassDirection4.kt
+++ src/util/CompassDirection4.kt
@@ -9,7 +9,10 @@ enum class CompassDirection4 {
     NORTH,EAST,SOUTH,WEST;
 
     fun rotateClockwise():CompassDirection4 = VALUES[(this.ordinal+1) % VALUES.size]
-    fun rotateAntiClockwise():CompassDirection4 = VALUES[max((this.ordinal-1) % VALUES.size, 0)]
+    fun rotateAntiClockwise():CompassDirection4 = when(this){
+        NORTH -> WEST
+        else -> VALUES[max((this.ordinal-1) % VALUES.size, 0)]
+    }
 
     companion object {
         val VALUES = CompassDirection4.values()
